656dc6ca579760058cf55260ffcebf939abec243
kuttsun <tsukushimail+github@gmail.com>
2018-03-21 22:47:36 +0900

    Add CI

---
+++ src/OpenCover.bat
@@ -0,0 +1,16 @@
+﻿set PROJECT=%1
+
+set OPENCOVER="%USERPROFILE%\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe"
+
+rem Test command
+set TARGET=dotnet.exe
+
+rem Test command argument
+set TARGET_TEST="test %PROJECT%\%PROJECT%.csproj"
+
+rem OpenCover output
+set OUTPUT=OpenCover.xml
+
+set FILTERS="+[*]*"
+
+%OPENCOVER% -register:user -target:%TARGET% -targetargs:%TARGET_TEST% -filter:%FILTERS% -oldstyle -output:%OUTPUT%
---
+++ src/ReportGenerator.bat
@@ -0,0 +1,9 @@
+﻿set REPORTGEN="%USERPROFILE%\.nuget\packages\ReportGenerator\3.1.2\tools\ReportGenerator.exe"
+
+rem OpenCover output
+set OUTPUT="OpenCover.xml"
+
+rem ReportGenerator output
+set OUTPUT_DIR="ReportGenerator"
+
+%REPORTGEN% -reports:%OUTPUT% -targetdir:%OUTPUT_DIR%
