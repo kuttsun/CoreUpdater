7c05b56d244ad85a6f3af6bd9701930500cf44a1
kuttsun <tsukushimail+github@gmail.com>
2018-03-22 23:41:31 +0900

    Add CoreUpdater.Sample

---
+++ src/CoreUpdater.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreUpdater.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
